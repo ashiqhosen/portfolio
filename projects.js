0273973db0f4306e6941ff8dc61f07d9feda2dd4
ashiqhosen <ashiqhosenbd@gmail.com>
2025-08-14 23:41:08 +0600

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,12 @@
+export const projects = [
+    {
+        name:'https://devsoffice.com',
+        plugins:['learndash','membership','woocommerce'],
+        type:'Education', industry:'E-learning'
+    },
+    {
+        name:'https://dev.com',
+        plugins:['tutor lms','memberpress','store engine'],
+        type:'Education', industry:'E-learning'
+    },
+];
---
+++ synonyms.js
@@ -0,0 +1,8 @@
+export const synonyms = {
+  learndash: ['learndash','লার্নড্যাশ','লাউন্ডেশ','ld'],
+  membership: ['membership','মেম্বারশিপ','member'],
+  tutorlms: ['tutorlms','tutor lms','tutor'],
+  woocommerce: ['woocommerce','woo','wc'],
+  education: ['education','শিক্ষা','edu'],
+  corporate: ['corporate','কোম্পানি']
+};
